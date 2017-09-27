15c3ba67fce8c4a60e6b0dc2a901d0358cffe294
Ademílson F. Tonato <contato@admcode.com.br>
2017-09-27 12:07:03 -0300

    update script methods

--- quoteapp/scripts/app.js
+++ quoteapp/scripts/app.js
@@ -1,1 +1,41 @@
-/* TODO add js here */
+(function() {
+  'use strict';
+
+  var app = {
+    isLoading: true,
+    selectedQuotes: [],
+    spinner: document.querySelector('.loader'),
+    lastId: 0
+  };
+
+  document.getElementById('reload').addEventListener('click', function() {
+    // Refresh all of the quotes
+    //app.updateQuotes();
+  });
+
+  // TODO add update card method here
+  app.updateQuoteCard = function(data) {
+    var text = document.querySelector('.text')
+    , author = document.querySelector('.author');
+
+    text.innerHTML = data.text;
+    author.innerHTML = data.author;
+  };
+
+  /*
+   * Fake quote data that is presented when the user first uses the app
+   */
+  var initialQuote = {
+    text: 'The future of advertising is the Internet.',
+    author: 'Bill Gates'
+  };
+  // TODO uncomment line below to test app with fake data
+  // app.updateQuoteCard(initialQuote);
+
+  // TODO add startup code here
+
+  // TODO add service worker code here
+  if ('serviceWorker' in navigator) {
+    // navigator.serviceWorker.register('./service-worker.js').then(function() { console.log('Service Worker Registered'); });
+  }
+})();
